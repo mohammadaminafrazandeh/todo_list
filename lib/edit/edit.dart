20e5a23276c44dd043adc8ba2c87c6de9d22d5d1
Amin <124354256+xzierox@users.noreply.github.com>
2024-02-09 16:16:27 +0330

    flutter bloc added to dependecies ready for creating folder for bloc

--- lib/edit/edit.dart
+++ lib/edit/edit.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_list/data/model/data.dart';
 import 'package:todo_list/main.dart';
