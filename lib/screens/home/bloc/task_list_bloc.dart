d3e4ca7413090b862337f6c35232c054cf315ace
Amin <124354256+xzierox@users.noreply.github.com>
2024-02-12 19:21:56 +0330

    bloc for home is completed

--- lib/screens/home/bloc/task_list_bloc.dart
+++ lib/screens/home/bloc/task_list_bloc.dart
@@ -1,13 +1,39 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:todo_list/data/model/data.dart';
+import 'package:todo_list/repository/repository.dart';
 
 part 'task_list_event.dart';
 part 'task_list_state.dart';
 
 class TaskListBloc extends Bloc<TaskListEvent, TaskListState> {
-  TaskListBloc() : super(TaskListInitial()) {
-    on<TaskListEvent>((event, emit) {
-      if (event is TaskListSearch) {}
+  final Repository<TaskEntity> repository;
+  TaskListBloc(this.repository) : super(TaskListInitial()) {
+    on<TaskListEvent>((event, emit) async {
+      if (event is TaskListStarted || event is TaskListSearch) {
+        emit(TaskListLoading());
+        final String searchTerm;
+        if (event is TaskListSearch) {
+          searchTerm = event.searchTerm;
+        } else {
+          searchTerm = '';
+        }
+
+        try {
+          final List<TaskEntity> items;
+          items = await repository.getAll(searchKeyword: searchTerm);
+          if (items.isEmpty) {
+            emit(TaskListEmpty());
+          } else {
+            emit(TaskListSuccess(tasks: items));
+          }
+        } catch (e) {
+          emit(TaskListError(message: 'Error Massage : $e'));
+        }
+      } else if (event is TaskListDeleteAll) {
+        await repository.deleteAll();
+        emit(TaskListEmpty());
+      }
     });
   }
 }
